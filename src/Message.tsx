5e7d511d734a504444204e48ba93daf82377462b
Aerry <aerryasmani@gmail.com>
2024-01-06 15:54:37 +0800

    This is my first attempt to create a portfolio

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//PascalCasing : initial caps for code
+function Message() {
+    const name ='Astraea'
+    if (name)
+     return <h1>Hello {name}</h1>;
+    return <h1>Hello Anon</h1>;
+}
+
+export default Message;
