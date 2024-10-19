1b4d48c5739395d1797030892e90052bcd461543
Aerry Asmani <aerryasmani@gmail.com>
2024-10-19 10:53:48 +0800

    Modify the html.index file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import 'boostrap/dist/css/boostrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
