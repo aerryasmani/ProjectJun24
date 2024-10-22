fbea1298688d215885dfafa95e2baf3bf5eae6ee
Aerry Asmani <aerryasmani@gmail.com>
2024-10-22 10:07:20 +0800

    Modify the responsiveness in mobile

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-//PascalCasing : initial caps for code
-function Message() {
-    const name ='Astraea'
-    if (name)
-     return <h1>Hello {name}</h1>;
-    return <h1>Hello Anon</h1>;
-}
-
-export default Message;
